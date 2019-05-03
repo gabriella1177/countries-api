1aacab19833a1d8940594c01da4062c0f2814b63
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-05-03 13:56:06 -0400

    cleaned up comments on getCountries.js

--- db/getCountries.js
+++ db/getCountries.js
@@ -18,25 +18,3 @@ axios.get(url)
 .catch(function(){
   console.log("ERR")
 })
-
-
-
-// // execute all promises, writing to disk if successful
-// Promise.all(calls)
-// .then(success => {
-// 	let collectedData = success.map(res => res.data)
-// 	let stringified = JSON.stringify(collectedData)
-// 	fs.writeFile(__dirname + '/countries.json', stringified, 'utf8', (err) => {
-// 		if(err) {
-// 			console.error(err)
-// 		}
-// 		else {
-// 			console.log(`successfully wrote ${collectedData.length} records to db/countries.json`)
-// 		}
-// 	}) 
-// })
-// .catch(err => {
-// 	console.error(err)
-// 	console.error('there was probably an issue with the rate limit, try again in 10 seconds or check the error messages above.')
-// })
-
